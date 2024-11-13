fb9d7eddd0f28bf3eef136c890e80323a2669127
jeff <jeffmadro@gmail.com>
2024-11-12 23:53:02 -0700

    initialization

--- errorProgagation.py
+++ errorProgagation.py
@@ -1,15 +1,6 @@
 import math as m
 
-def findError():
-    type = input("m for multiplication a for addition or d for division: ")
-    x =  float(input("what is the first value: "))
-    dx = float(input("what is the associated error: "))
-    y =  float(input("what is the second value: "))
-    dy = float(input("what is the second associated error: "))
-    output = float
-    o = tuple
-
-
+def findError(type, x, dx, y, dy):
     match type:
         case "m":
             output = m.abs(x*y) * m.sqrt((dx/x)**2 + (dy/y)**2)
@@ -25,7 +16,24 @@ def findError():
 
 
 def main():
-    o, output = findError()
+
+    type = input("m for multiplication a for addition or d for division: ")
+    x =  float(input("what is the first value: "))
+    dx = float(input("what is the associated error: "))
+    y =  float(input("what is the second value: "))
+    dy = float(input("what is the second associated error: "))
+    output = float
+    o = tuple
+    o, output = findError(type, x, dx, y, dy)
+
+    print(f"the error propogation for {o} is {output}")
+
+    # loop to get the values for repeated opperations
+    y =  float(input("what is the second value: "))
+    dy = float(input("what is the second associated error: "))
+    o, output = findError(type, x, dx, y, dy)
+
+    
     print(f"the error propogation for {o} is {output}")
 
 
